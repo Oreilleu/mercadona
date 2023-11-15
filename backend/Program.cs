14e34bcaead0236a84e33bf990bbe9ab36bb198a
oreille <aurelien.picardpabijan@gmail.com>
2023-11-15 13:45:35 +0100

    Condition database

--- backend/Program.cs
+++ backend/Program.cs
@@ -90,7 +90,10 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<DataContext>();
 
-    context.Database.Migrate();
+    if (context.Database.CanConnect())
+    {
+        context.Database.Migrate();
+    }
 
     if (!context.Categories.Any())
     {
